7055dfd1842412329ce2df4649296a9a80a15dde
rin <chenmfwyyx@163.com>
2024-08-20 22:46:35 +0800

    Site updated: 2024-08-20 22:46:34

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1723944483784";
+const VERSION = "1724165191675";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
