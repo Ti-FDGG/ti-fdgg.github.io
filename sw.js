92858439b4e3f4dd1a8c09869b62a80dd09c1aa3
Timothy <chenmfwyyx@163.com>
2025-07-06 13:24:30 +0800

    Site updated: 2025-07-06 13:24:29

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1751710725036";const preCache = ["/images/taichi.png","/images/banner/new_ban.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1751779464983";const preCache = ["/images/taichi.png","/images/banner/new_ban.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
